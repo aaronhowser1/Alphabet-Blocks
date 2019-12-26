ca57c2f0dece2990ef3c4c643279e4f2bc4d9f8d
Aaron Howser <aaronhowser1@gmail.com>
2019-12-26 04:58:47 -0600

    Broken A

---
+++ src/main/java/com/aaronhowser1/alphabetblocks/AlphabetBlocks.java
@@ -0,0 +1,54 @@
+package com.aaronhowser1.alphabetblocks;
+
+import com.aaronhowser1.alphabetblocks.blocks.ModBlocks;
+import com.aaronhowser1.alphabetblocks.blocks.BlockBigA;
+import com.aaronhowser1.alphabetblocks.setup.ClientProxy;
+import com.aaronhowser1.alphabetblocks.setup.IProxy;
+import com.aaronhowser1.alphabetblocks.setup.ServerProxy;
+import net.minecraft.block.Block;
+import net.minecraft.item.BlockItem;
+import net.minecraft.item.Item;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.DistExecutor;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod("alphabetblocks")
+public class AlphabetBlocks {
+
+    private static final Logger LOGGER = LogManager.getLogger();
+
+    public static IProxy proxy = DistExecutor.runForDist(() -> () -> new ClientProxy(), () -> () -> new ServerProxy());
+
+    public AlphabetBlocks() {
+        // Register the setup method for modloading
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+
+        // Register ourselves for server and other game events we are interested in
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void setup(final FMLCommonSetupEvent event) {
+    }
+
+    // You can use EventBusSubscriber to automatically subscribe events on the contained class (this is subscribing to the MOD
+    // Event bus for receiving Registry Events)
+    @Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD)
+    public static class RegistryEvents {
+        @SubscribeEvent
+        public static void onBlocksRegistry(final RegistryEvent.Register<Block> event) {
+            event.getRegistry().register(new BlockBigA());
+        }
+
+        @SubscribeEvent
+        public static void onItemsRegistry(final RegistryEvent.Register<Item> event) {
+            event.getRegistry().register(new BlockItem(ModBlocks.BLOCKBIGA, new Item.Properties()).setRegistryName("big_a"));
+        }
+    }
+}
---
+++ src/main/java/com/aaronhowser1/alphabetblocks/blocks/ModBlocks.java
@@ -0,0 +1,9 @@
+package com.aaronhowser1.alphabetblocks.blocks;
+
+import net.minecraftforge.registries.ObjectHolder;
+
+public class ModBlocks {
+
+    @ObjectHolder("alphabetblocks:a")
+    public static BlockBigA BLOCKBIGA;
+}
